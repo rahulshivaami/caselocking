1ff80d58d199ec1c3578f8e1bb85f91ad03cbcd1
rahul bhalekar <rahul.b@shivaami.com>
2020-10-13 12:09:43 +0530

    new

--- menu.php
+++ menu.php
@@ -4,8 +4,9 @@ require 'header.php';
 <style>
     /* Add a black background color to the top navigation */
     .topnav {
-        background-color: #333;
+        background-color: #2cace3;
         overflow: hidden;
+        
     }
 
     /* Style the links inside the navigation bar */
@@ -13,21 +14,22 @@ require 'header.php';
         float: left;
         color: #f2f2f2;
         text-align: center;
-        padding: 10px 16px;
+        padding: 10px 36px;
         text-decoration: none;
         font-size: 17px;
+        margin-left:5px;
     }
 
     /* Change the color of links on hover */
     .topnav a:hover {
-        background-color: #4CAF50;
-        color: #fff;
+        background-color: #fff;
+        color: #2cace3;
     }
 
     /* Add a color to the active/current link */
     .topnav a.active {
-        background-color: #4CAF50;
-        color: white;
+        background-color: #fff;
+        color: #2cace3;
     }
 
     /* Right-aligned section inside the top navigation */
@@ -38,7 +40,9 @@ require 'header.php';
 <div class="container-fluid">
     <div class="row">
         <div class="topnav">
+            <img style="float:left;height: 45px;padding-right:15px;" src="images/shivaamiLogo.png" />
             <a class="active" href="home.php">Home</a>
+            <a class="" href="#">Detail</a>
             <div class="topnav-right">
                 <a href="logout.php">Logout</a>
             </div>
